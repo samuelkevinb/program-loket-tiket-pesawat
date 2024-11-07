9ad35a68da8347f2d5f5655652b06b16b7b38fa2
Samuel Kevin Bagaskara <154964334+samuelkevinb@users.noreply.github.com>
2024-11-07 21:26:21 +0700

    Update Program Loket Tiket Pesawat.py

--- Program Loket Tiket Pesawat.py
+++ Program Loket Tiket Pesawat.py
@@ -1,65 +1,101 @@
-print ("\nSelamat Datang Di Pesawat Penerbangan")
-print ("============SAM FLIGHT AIR============")
+print("\nSelamat Datang Di Pesawat Penerbangan")
+print("============ SAM FLIGHT AIR ============")
 print()
-print (" Kode Penerbangan   |   Tujuan  |   Harga Tiket  ")
-print ("-------------------------------------------------")
-print ("101                 |   ENG     |   RP. 5.000.000")
-print ("102                 |   BALI    |   RP. 1.500.000")
-print ("103                 |   USA     |   RP. 5.500.000")
-print ("104                 |   JPN     |   RP. 2.000.000")
-print ("105                 |   KOR     |   RP. 3.000.000")
-print ("-------------------------------------------------")
-print ("\n")
-print ("============ FORM DATA SAM FLIGHT AIR ============")
+
+# Memilih Maskapai
+print("Pilih Maskapai Penerbangan:")
+print("1. Air Asia")
+print("2. Citilink")
+print("3. Garuda Indonesia")
+
+maskapai = input("Masukkan Pilihan Maskapai (1/2/3): ")
+
+if maskapai == "1":
+    nama_maskapai = "Air Asia"
+    kode_penerbangan = {
+        "101": ("ENGLAND", 7000000),
+        "102": ("BALI", 3000000),
+        "103": ("USA", 8000000),
+        "104": ("JAPAN", 5000000),
+        "105": ("KOREA", 6000000)
+    }
+elif maskapai == "2":
+    nama_maskapai = "Citilink"
+    kode_penerbangan = {
+        "201": ("ENGLAND", 4000000),
+        "202": ("BALI", 1200000),
+        "203": ("USA", 4500000),
+        "204": ("JAPAN", 1700000),
+        "205": ("KOREA", 2500000)
+    }
+elif maskapai == "3":
+    nama_maskapai = "Garuda Indonesia"
+    kode_penerbangan = {
+        "301": ("ENGLAND", 10000000),
+        "302": ("BALI", 5000000),
+        "303": ("USA", 11000000),
+        "304": ("JAPAN", 7000000),
+        "305": ("KOREA", 8500000)
+    }
+else:
+    print("Pilihan maskapai tidak valid.")
+    exit()
+
+print(f"\nAnda memilih maskapai: {nama_maskapai}")
+print("\n Kode Penerbangan   |   Tujuan  |   Harga Tiket  ")
+print("-------------------------------------------------")
+for kode, (tujuan, harga) in kode_penerbangan.items():
+    print(f"{kode:<20} | {tujuan:<8} | RP. {harga:,}")
+print("-------------------------------------------------")
+print("\n")
+print("============ FORM DATA SAM FLIGHT AIR ============")
 pembeli = str(input("Nama = "))
 notelp = int(input("Nomor Telepon = "))
 tujuan = input("Kode Tujuan Penerbangan = ")
 
-if tujuan == "101":
-    print("Tujuan Penerbangan = ENGLAND")
-    namatujuan = "ENGLAND"
-    harga = 5000000
-    
-elif tujuan == "102":
-    print ("Tujuan Penerbangan = BALI")
-    namatujuan = "BALI"
-    harga = 1500000
-
-elif tujuan == "103":
-    print ("Tujuan Penerbangan = UNITED STATE AMERICA")
-    namatujuan = "UNITED STATE AMERICA"
-    harga = 5500000
-
-elif tujuan == "104":
-    print ("Tujuan Penerbangan = JAPAN")
-    namatujuan = "JAPAN"
-    harga = 2000000
-
-elif tujuan == "105":
-    print ("Tujuan Penerbangan = KOREA")
-    namatujuan = "KOREA"
-    harga = 3000000
-
-else :
-    print ("Kode Penerbangan tidak ada")
-    
+# Mengecek kode penerbangan dan menentukan harga
+if tujuan in kode_penerbangan:
+    namatujuan, harga = kode_penerbangan[tujuan]
+    print(f"Tujuan Penerbangan = {namatujuan}")
+else:
+    print("Kode Penerbangan tidak ada")
+    exit()
 
 jumlahbeli = int(input("Jumlah Pembelian Tiket = "))
 
-total = int(jumlahbeli * harga)
+# Menghitung total harga sebelum diskon
+total = jumlahbeli * harga
 
+# Cek jika mendapatkan diskon
+diskon = 0
+if jumlahbeli > 3:
+    diskon = 0.1 * total  # Diskon 10%
+    print(f"Selamat! Anda mendapatkan diskon 10% karena membeli lebih dari 3 tiket.")
+else:
+    print("Pembelian kurang dari 3 tiket, tidak mendapatkan diskon.")
+
+# Total setelah diskon
+total_setelah_diskon = total - diskon
+
+# Menampilkan detail transaksi
 print("=======================================")
 print("  MASKAPAI SAM FLIGHT AIR  ")
 print("=======================================")
-print("Nama Pembeli         : " +str(pembeli))
-print("No. Handphone        : " +str(notelp))
-print("Kode Penerbangan     : " +str(tujuan))
-print("Tujuan Penerbangan   : " +str(namatujuan))
-print("Harga Tiket          : " +str(harga))
-print("Jumlah Beli Tiket    : " +str(jumlahbeli))
-print("Total Pembayaran     : " +str(total))
+print("Nama Maskapai       : " + str(nama_maskapai))
+print("Nama Pembeli        : " + str(pembeli))
+print("No. Handphone       : " + str(notelp))
+print("Kode Penerbangan    : " + str(tujuan))
+print("Tujuan Penerbangan  : " + str(namatujuan))
+print("Harga Tiket         : RP. " + "{:,}".format(harga))
+print("Jumlah Beli Tiket   : " + str(jumlahbeli))
+print("Total Pembayaran    : RP. " + "{:,}".format(total))
+if diskon > 0:
+    print("Diskon              : RP. " + "{:,}".format(int(diskon)))
+    print("Keterangan Diskon   : Anda mendapatkan diskon 10% karena membeli lebih dari 3 tiket.")
+print("Total Setelah Diskon: RP. " + "{:,}".format(int(total_setelah_diskon)))
 print("=======================================")
 
+# Pembayaran dan kembalian
 bayar = int(input("Masukan Uang Pembayaran Anda : "))
-kembalian = int(bayar - total)
-print("Uang Kembalian : " +str(kembalian))
+kembalian = bayar - total_setelah_diskon
+print("Uang Kembalian : RP. " + "{:,}".format(int(kembalian)))
